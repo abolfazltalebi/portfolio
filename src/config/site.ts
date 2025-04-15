b41835a62075cba7463083244cbf370c05cc0143
Your Name <you@example.com>
2025-04-15 16:25:48 +0330

    config project and more....

---
+++ src/config/site.ts
@@ -0,0 +1,57 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Vite + HeroUI",
+  description: "Make beautiful websites regardless of your design experience.",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "about me",
+      href: "/docs",
+    },
+    {
+      label: "skills",
+      href: "/",
+    },
+    {
+      label: "my project",
+      href: "/",
+    },
+    {
+      label: "contact me",
+      href: "/contact me",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "about me",
+      href: "/docs",
+    },
+    {
+      label: "skills",
+      href: "/",
+    },
+    {
+      label: "my project",
+      href: "/",
+    },
+    {
+      label: "contact me",
+      href: "/contact me",
+    },
+  ],
+  links: {
+    github: "https://github.com/frontio-ai/heroui",
+    twitter: "https://twitter.com/hero_ui",
+    docs: "https://heroui.com",
+    discord: "https://discord.gg/9b6yyZKmH4",
+    sponsor: "https://patreon.com/jrgarciadev",
+  },
+};
